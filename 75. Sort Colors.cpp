590f20d1397c1a9f47d63a3c82fb47527575c17b
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-05-23 17:17:40 +0300

    Update 75. Sort Colors.cpp

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -10,3 +10,23 @@ public:
         }
     }
 };
+
+//another solution 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //using counting sort
+        int count[3]={0,0,0};
+        int extra[nums.size()];
+        for(int i=0;i<nums.size();i++) {
+            count[nums[i]]++;
+            extra[i]=nums[i];
+        }
+        for(int i=1;i<3;i++) {
+            count[i]+=count[i-1];
+        }
+        for(int i=nums.size()-1;i>=0;i--) {
+            nums[--count[extra[i]]] = extra[i];
+        }
+    }
+};
