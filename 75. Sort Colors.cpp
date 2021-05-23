3177d319588b5b6f22274f059dba8409a17225f8
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-05-23 17:22:01 +0300

    Update 75. Sort Colors.cpp

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //BUBBLE SORT
     void sortColors(vector<int>& nums) {
         for(int i=0;i<nums.size()-1;i++) {
             for(int j=i+1;j<nums.size();j++) {
@@ -11,7 +12,7 @@ public:
     }
 };
 
-//another solution 
+//another solution COUNTING SORT
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -28,5 +29,38 @@ public:
         for(int i=nums.size()-1;i>=0;i--) {
             nums[--count[extra[i]]] = extra[i];
         }
+    }
+};
+
+////COCKTAIL SORT
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //using cocktail sort
+        bool isSwapped = true;
+        int start =0;
+        int end = nums.size();
+        while(isSwapped) {
+            isSwapped=false;
+            for(int i=start;i<end-1;i++) {
+                if(nums[i]>nums[i+1]) {
+                    swap(nums[i],nums[i+1]);
+                    isSwapped=true;
+                }
+            }
+            if(!isSwapped) break;
+            end--;   
+            isSwapped=false;
+            for(int i=end-1;i>=start;i--) {
+                 if(nums[i]>nums[i+1]) {
+                    swap(nums[i],nums[i+1]);
+                    isSwapped=true;
+                }
+            }
+            start++;
+            
+            
+        }
+        
     }
 };
