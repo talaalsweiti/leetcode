44d2b83bc6ddcbdae9e6a5d89aee95be974d2866
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-05-31 15:43:00 +0300

    Update 242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -6,3 +6,22 @@ public:
         return s==t;
     }
 };
+
+/////////////////////////
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> occurencesSTR1 ;
+        map<char,int> occurencesSTR2 ;
+        for(int i=0;i<s.length();i++) occurencesSTR1[s[i]]++;
+        for(int i=0;i<t.length();i++)
+        {
+            occurencesSTR2[t[i]]++;
+            if(occurencesSTR1[t[i]]==0) return false;
+        }
+        for(char a='a';a<='z';a++){
+            if(occurencesSTR1[a]!=occurencesSTR2[a]) return false;
+        }
+        return true;
+    }
+};
