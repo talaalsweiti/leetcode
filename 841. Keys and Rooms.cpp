c93d8b4948f9e48b15efb037a0b78cc03e355ddc
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-07-02 11:08:42 +0300

    Create 841. Keys and Rooms.cpp

---
+++ 841. Keys and Rooms.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int,bool> visited;
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+       dfs(rooms,0);
+       for(int i =0;i< rooms.size() ;i++) {
+           if(!visited[i]) return false;
+       }
+        return true;
+    }
+    void dfs(vector<vector<int>>& rooms , int node) {
+        visited[node] = true;
+        for(int i=0;i<rooms[node].size();i++ ) {
+            if(visited[rooms[node][i]]==false) {
+                dfs(rooms,rooms[node][i]);
+            }
+        }
+    }
+};
