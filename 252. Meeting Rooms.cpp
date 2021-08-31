fa5c56f82b8549d42c45fe245e94de77b17fdb98
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-08-31 12:45:59 +0300

    Create 252. Meeting Rooms.cpp

---
+++ 252. Meeting Rooms.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if(!intervals.size()) return true;
+        sort(intervals.begin(),intervals.end());
+        priority_queue<pair<int,int>> maxHeap;
+        maxHeap.push({intervals[0][1],intervals[0][0]});
+        for(int i=1;i<intervals.size();i++) {
+            pair<int,int> top = maxHeap.top();
+            if(top.first>intervals[i][0]){
+                maxHeap.pop();
+                top.second = min(intervals[i][0],top.second);
+                top.first = max(intervals[i][1],top.first);
+                maxHeap.push(top);
+            }
+            else{
+                maxHeap.push({intervals[i][1],intervals[i][0]});
+            }
+        }
+        return maxHeap.size()==intervals.size();
+    }
+};
