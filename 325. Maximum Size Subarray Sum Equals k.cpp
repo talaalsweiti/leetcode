5d5cb38f727637c1d07cea42f9185d2ad353a44e
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-09-11 23:15:47 +0300

    Update 325. Maximum Size Subarray Sum Equals k.cpp

--- 325. Maximum Size Subarray Sum Equals k.cpp
+++ 325. Maximum Size Subarray Sum Equals k.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        vector<int> prefixSum;
-        prefixSum.push_back(0);
-        int ans = INT_MAX;
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] =-1;
+        int sum =0;
+        int ans =0;
         for(int i=0;i<nums.size();i++) {
-            prefixSum.push_back(nums[i]);
-        }
-        
-        for(int i=1;i<prefixSum.size();i++) {
-            prefixSum[i]+=prefixSum[i-1];
-        }
-        int window = 0, left ,right = 1;
-        while(window<prefixSum.size()) {
-            left = 1 ;
-            for(int i=left,j=right;j<prefixSum.size();i++) {
-                int sum = prefixSum[j] - prefixSum[i-1]; 
-                if(sum>=target) return j-i+1;
-                j++;
+            sum+=nums[i];
+             if(mp.find(sum)==mp.end()){ //if there's duplicates we dont want to update the index
+                 //becuase we want the longest subbarray 
+                mp[sum] =i;
             }
-            right++ ,window++;
+            if(mp.find(sum-k)!=mp.end()){
+                ans = max(ans,i-mp[sum-k]);
+            }
+           
+           
         }
-        
-        return 0;
-        /* [2,3,1,2,4,3] 
-        0 2 5 6 8 12 15
-       */     
+        /*
+        [1,-1,5,-2,3]
+        0,1,0,5,3,6
+        mp[0] = 0
+        mp[1] = 1
+        mp[0] = 2
+        mp[5] = 3
+        mp[] ,4
+               4 - 2  = 2   
+        */
+       return ans;
     }
 };
