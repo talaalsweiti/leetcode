d1c9ccb124d72ee6b00bccd40d65e6c41798e25d
Tala Alsweiti <70711756+talaalsweiti@users.noreply.github.com>
2021-12-17 14:00:32 +0200

    Update 63. Unique Paths II.cpp

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> paths =  obstacleGrid;
         int m = obstacleGrid.size() , n = obstacleGrid[0].size();
-        if(paths[m-1][n-1] ||paths[0][0] ) return 0;
+        if(obstacleGrid[m-1][n-1] ||obstacleGrid[0][0] ) return 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(paths[i][j]==1){
-                    paths[i][j]=-1;
+                if(obstacleGrid[i][j]==1){
+                    obstacleGrid[i][j]=-1;
                 }
             }
         }
-        paths[0][0] =1;
+        obstacleGrid[0][0] =1;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++){
-                if(paths[i][j]==-1) continue;
-                if(i-1>=0 && paths[i-1][j]!=-1) paths[i][j]+=paths[i-1][j];
-                if(j-1>=0 && paths[i][j-1]!=-1) paths[i][j]+=paths[i][j-1];
+                if(obstacleGrid[i][j]==-1) continue;
+                if(i-1>=0 && obstacleGrid[i-1][j]!=-1) obstacleGrid[i][j]+=obstacleGrid[i-1][j];
+                if(j-1>=0 && obstacleGrid[i][j-1]!=-1) obstacleGrid[i][j]+=obstacleGrid[i][j-1];
             }
         }
-        return paths[m-1][n-1];
+        return obstacleGrid[m-1][n-1];
     }
 };
 /*
