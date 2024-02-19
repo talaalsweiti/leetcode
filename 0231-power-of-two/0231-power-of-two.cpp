40baa9eb0dac51028a6a679a3aa18f668bea52f8
Tala Alswaitti <70711756+talaalsweiti@users.noreply.github.com>
2024-02-19 21:35:58 +0200

    Time: 0 ms (100.00%), Space: 7.3 MB (19.56%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for(int i=0;i<32;i++){
+            if(pow(2,i)==n) return true;
+        }
+        
+        return false;
+    }
+};
+
+/*
+
+1 2 4 8 16 
+
+
+*/
