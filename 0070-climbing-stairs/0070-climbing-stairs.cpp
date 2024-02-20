4213cab70c79cce95bcf7412ddd8c16e15ff519a
Tala Alswaitti <70711756+talaalsweiti@users.noreply.github.com>
2024-02-20 21:54:42 +0200

    Time: 3 ms (16.05%), Space: 8.5 MB (6.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
+    map<int,int> sum;
     int climbStairs(int n) {
-        vector<int> memo(46,0);
-        memo[1] =1;
-        memo[2] =2;
-        for(int i=3;i<=n;i++){
-            memo[i]=memo[i-1]+memo[i-2];
-        }
-        return memo[n];
+        sum[1] = 1;
+        sum[2] = 2;
+        if(sum[n]) return sum[n];
+        sum[n] = climbStairs(n-1) + climbStairs(n-2);
+        return  sum[n] ; 
     }
 };
 
 /*
 
-1 -> 1
 
-2 -> 2
-
-
-3 -> 3
-
-
-4 -> 3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+1 - 1
+2 - 2
+3 - 3
+4 - 5 + 1
+5 - 8 + 3
+6 - 13 + 7
+7 - 21 + 14
+8 - 34 + 26
+9 - 55
+10 - 89
 
 
 */
