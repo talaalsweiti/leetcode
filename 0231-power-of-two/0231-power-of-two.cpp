b06f047712b90c33f32509b2eeb5e28b39b22993
Tala Alswaitti <70711756+talaalsweiti@users.noreply.github.com>
2024-02-29 20:40:10 +0200

    Time: 5 ms (19.41%), Space: 7.1 MB (87.40%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1) return true;
         if(n<=0) return false;
-         //(int)log2(n)+1 represnts the number of bits needed for n
-        int bits = (int)log2(n)+1;
-        return pow(2,bits-1)==n;
+        return !(n&(n-1));
     }
 };
 
